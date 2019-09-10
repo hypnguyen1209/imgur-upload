956a01a840b40feb3653f9edbfdb5d2a34ae6106
Nguyen Van Hiep <hiep12092001@gmail.com>
2019-09-10 19:30:52 +0700

    commit

---
+++ js/upload.js
@@ -0,0 +1,13 @@
+var feedback = function(res) {
+    if (res.success === true) {
+        var get_link = res.data.link.replace(/^http:\/\//i, 'https://');
+        document.querySelector('.status').classList.add('bg-success');
+        document.querySelector('.status').innerHTML =
+            ' ' + '<br><input class="image-url" value=\"' + get_link + '\"/>' + '<img class="img" alt="Imgur-Upload" src=\"' + get_link + '\"/>';
+    }
+};
+
+new Imgur({
+    clientid: 'b4a1c4bd0ea5845', //You can change this ClientID
+    callback: feedback
+});
